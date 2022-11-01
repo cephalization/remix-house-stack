9af54c9db91f5bb665dabfc46be15328a652fef9
Anthony Powell <anthonypowell@hey.com>
2022-10-31 21:19:30 -0400

    Fix lint and clean commands

--- apps/frontend/app/routes/index.tsx
+++ apps/frontend/app/routes/index.tsx
@@ -1,10 +1,33 @@
+import { json, type LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+type Health = {
+  ok: boolean;
+  error?: Error;
+};
+
+const API = "http://localhost:5001/healthz";
+
+export const loader: LoaderFunction = async () => {
+  try {
+    const res = await fetch(API);
+
+    return json((await res.json()) as Health);
+  } catch (e) {
+    return json({ ok: false, error: e });
+  }
+};
+
 export default function Index() {
+  const data = useLoaderData<Health>();
+
   return (
     <div className="container">
       <h1 className="title">
         Frontend <br />
         <span>Remix House Stack</span>
       </h1>
+      <p>The API is {data?.ok ? "Running" : "Not Running"}</p>
       <p className="description">
         Built With <a href="https://turbo.build/repo">Turborepo</a> +{" "}
         <a href="https://remix.run/">Remix</a>
